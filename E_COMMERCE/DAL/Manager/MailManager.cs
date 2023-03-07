99173a7e6a56814ad27228e29890d2bf040745d7
Rekha <rekharajankr@gmail.com>
2023-03-07 11:50:24 +0530

    Dal manager

---
+++ E_COMMERCE/DAL/Manager/MailManager.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Configuration;
+using System.Net.Http;
+using System.Net.Mail;
+
+namespace DAL.Manager
+{
+    public class MailManager
+    {
+
+        public string SendEmailId(Ent_Mails mld)
+        {
+            SmtpSection smtpSection = ConfigurationManager.GetSection("system.net/mailSettings/smtp") as SmtpSection;
+
+            using (MailMessage mm = new MailMessage(smtpSection.From, mld.Email))
+            {
+
+                mm.Subject = mld.Subject;
+                mm.Body = mld.Body;
+
+                mm.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = smtpSection.Network.Host;
+                    smtp.EnableSsl = smtpSection.Network.EnableSsl;
+                    NetworkCredential networkCred = new NetworkCredential(smtpSection.Network.UserName, smtpSection.Network.Password);
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = networkCred;
+                    smtp.Port = smtpSection.Network.Port;
+                    smtp.Send(mm);
+                }
+            }
+
+            return "Email sent sucessfully.";
+        }
+
+
+
+        public string AdminSendMail(Ent_Mails mld)
+        {
+            SmtpSection smtpSection = (SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
+
+            using (MailMessage mm = new MailMessage(mld.Email,smtpSection.From))
+            {
+
+                mm.Subject = mld.Subject;
+                mm.Body = mld.Body;
+
+                mm.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = smtpSection.Network.Host;
+                    smtp.EnableSsl = smtpSection.Network.EnableSsl;
+                    NetworkCredential networkCred = new NetworkCredential(smtpSection.Network.UserName, smtpSection.Network.Password);
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = networkCred;
+                    smtp.Port = smtpSection.Network.Port;
+                    smtp.Send(mm);
+                }
+            }
+
+            return "Email sent sucessfully.";
+        }
+
+    }
+}
--- E_COMMERCE/DAL/Manager/OrderManager.cs
+++ E_COMMERCE/DAL/Manager/OrderManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,82 @@ using System.Threading.Tasks;
 
 namespace DAL.Manager
 {
-    class OrderManager
+    public class OrderManager
     {
+        E_COMMERCEEntities dbhelper = new E_COMMERCEEntities();
+        public string AddOrder(ORDER ord)
+        {
+            int result = 0;
+
+            dbhelper.ORDERS.Add(ord);
+            result = dbhelper.SaveChanges();
+
+            if (result > 0)
+                return ord.ORD_ID.ToString();
+            else
+                return "Error ";
+        }
+
+        public List<ORDER> Product_OrderDetails(int id)
+        {
+            return dbhelper.ORDERS.Where(p => p.ORD_PROID == id).ToList();
+        }
+
+        public List<ORDER> Ordered_UserDetails(ORDER uid)
+        {
+            return dbhelper.ORDERS.Where(p => p.ORD_USERID == uid.ORD_USERID && p.ORD_STATUS != "D").ToList();
+        }
+
+        public void OrderCancelation(ORDER obj)
+        {
+            var id = dbhelper.ORDERS.SingleOrDefault(e => e.ORD_ID == obj.ORD_ID);
+
+            dbhelper.ORDERS.Remove(id);
+            dbhelper.SaveChanges();
+        }
+
+        public string SelectMailid(ORDER order)
+        {
+            var mailid = dbhelper.USERs.FirstOrDefault(e => e.USER_ID == order.ORD_USERID);
+
+            return mailid.USER_EMAIL;
+        }
+
+        public int GetPrice(ORDER rd)
+        {
+            var tot = dbhelper.PRODUCTS.FirstOrDefault(e => e.PRO_ID == rd.ORD_PROID);
+            return tot.PRO_PRICE;
+        }
+
+        public int GetPriceByProduct(int proid)
+        {
+            var tot = dbhelper.PRODUCTS.FirstOrDefault(e => e.PRO_ID == proid);
+            return tot.PRO_PRICE;
+        }
+
+        public List<ORDER> OrderByDate(ORDER obj)
+        {
+            return dbhelper.ORDERS.Where(p => p.ORD_MODIDATE == obj.ORD_MODIDATE).ToList();
+        }
+
+        public List<ORDER> OrderFullData(ORDER obj)
+        {
+            return dbhelper.ORDERS.Where(p => p.ORD_USERID == obj.ORD_USERID).ToList();
+        }
+        public string UpdateStatus(ORDER obj)
+        {
+            var id = dbhelper.ORDERS.FirstOrDefault(p => p.ORD_USERID == obj.ORD_USERID);
+            if (id == null)
+                return "Error";
+            else
+            {
+                id.ORD_STATUS = obj.ORD_STATUS;
+
+                dbhelper.Entry(id).State = System.Data.Entity.EntityState.Modified;
+                dbhelper.SaveChanges();
+                return id.ORD_ID.ToString();
+            }
+        }
+
     }
 }
